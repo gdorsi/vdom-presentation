297264d4cbd8e7c2a5d31acbda4d34c497ea658c
fughye <gu.dorsi@gmail.com>
2017-03-13 22:46:58 +0100

    Step 2 - Rendering

--- index.js
+++ index.js
@@ -4,11 +4,31 @@ function wtf(nodeName, attributes, ...children) {
     return {nodeName, attributes, children};
 }
 
+function render(vnode) {
+    if (typeof vnode === 'string') {
+        return document.createTextNode(vnode);
+    }
+
+    let node = document.createElement(vnode.nodeName);
+
+    Object.keys(vnode.attributes || {}).forEach((name) => {
+        name in node
+            ? node[name] = vnode.attributes[name]
+            : node.setAttribute(name, vnode.attributes[name]);
+    });
+
+    (vnode.children || []).forEach((child) => {
+        node.appendChild(render(child));
+    });
+
+    return node;
+}
+
 let vdom = (
-    <div style="font-weight: bold;">
-        <span>WTF virtual dom is so</span>
-        <strong>is so cool!</strong>
+    <div style="font-size: 16px;">
+        <span>WTF virtual dom</span>
+        <strong> is so damn cool!</strong>
     </div>
 );
 
-console.log(vdom);
+document.body.appendChild(render(vdom));
