a4b99f5d895db9f225dac7194d94a1f08a2d02b3
fughye <gu.dorsi@gmail.com>
2017-03-13 23:11:33 +0100

    Step 4 - Dom diffing

--- index.js
+++ index.js
@@ -4,28 +4,69 @@ function wtf(nodeName, attributes, ...children) {
     return {nodeName, attributes, children};
 }
 
-function render(vnode) {
+function render(vnode, dom) {
     if (typeof vnode === 'string') {
+        if (dom instanceof Text) {
+            dom.nodeValue = vnode;
+            return dom;
+        }
+
         return document.createTextNode(vnode);
     }
 
     let {nodeName, attributes, children} = vnode;
 
+    attributes = attributes || {};
+    children = children || [];
+
     if (typeof nodeName === 'function') {
-        return render(nodeName(attributes || {}, children));
+        return render(nodeName(attributes, children), dom);
     }
 
-    let node = document.createElement(nodeName);
+    let node;
 
-    Object.keys(attributes || {}).forEach((name) => {
+    if (dom && dom.tagName && dom.tagName.toLowerCase() == nodeName) {
+        node = dom;
+    } else {
+        node = document.createElement(nodeName);
+    }
+
+    Object.keys(attributes).forEach((name) => {
         name in node
             ? node[name] = attributes[name]
             : node.setAttribute(name, attributes[name]);
     });
 
-    (children || []).forEach((child) => {
-        node.appendChild(render(child));
+    if (dom === node) {
+        Object.keys(node.__attrs || node.attributes).forEach((name) => {
+            if (!(name in attributes)) {
+                name in node
+                    ? node[name] = void(0)
+                    : node.removeAttribute(name);
+            }
+        });
+    }
+
+    node.__attrs = attributes;
+
+    children.forEach((child, i) => {
+        let prev = node.childNodes[i];
+        let next = render(child, prev);
+
+        if (prev !== next) {
+            if (prev) {
+                node.replaceChild(next, prev);
+            } else {
+                node.appendChild(next);
+            }
+        }
     });
+
+    Array.prototype.slice.call(node.childNodes, children.length).forEach(
+        (child) => {
+            child.parentNode.removeChild(child);
+        }
+    );
 
     return node;
 }
@@ -36,14 +77,18 @@ let AndComposable = ({makeItRed}) => (
     </span>
 );
 
-let Main = () => (
-    <div style="font-size: 16px;">
-        <span>WTF virtual dom is </span>
-        <strong>so damn cool </strong>
-        <AndComposable makeItRed={true} />
-    </div>
+let Main = ({size}) => (
+    <body>
+        <div style={`font-size: ${size || 16}px;`}>
+            <span>WTF virtual dom is </span>
+            <strong>so damn cool </strong>
+            <AndComposable makeItRed={true} />
+        </div>
+    </body>
 );
 
-let node = render(<Main />);
+render(<Main />, document.body);
 
-document.body.appendChild(node);
+setTimeout(() => {
+    render(<Main size={20}/>, document.body);
+}, 2000);
